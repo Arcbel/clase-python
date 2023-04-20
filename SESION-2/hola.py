e5a34df8547f19b4dc199fb50443799413bca5dd
Arcbel <valeria.rojasro@gmail.com>
2023-04-19 22:36:42 -0400

    primer commit

---
+++ SESION-2/hola.py
@@ -0,0 +1,14 @@
+print("Hola")
+
+edad = 29
+sueldo = 80000.00
+nombre = "Valeria"
+isTeacher = False
+
+print(edad, sueldo, nombre, isTeacher)
+
+numero_1 = 6.7
+numero_2 = 2.9
+print((numero_1 * numero_2) + numero_2**numero_1)
+
+holaaaa
